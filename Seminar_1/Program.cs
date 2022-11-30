a888877a49be2037f49f772ded90e5d79b3c01c7
Olga <olga84k@mail.ru>
2022-11-30 11:41:49 +0400

    max,min_2 number

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+string number1 = Console.ReadLine() ?? "";
+Console.WriteLine("Введите второе число");
+string number2 = Console.ReadLine() ?? "";
+int numberfirst = Convert.ToInt32(number1);
+int numbersekond = Convert.ToInt32(number2);
+int max  = numberfirst;
+int min = numbersekond;
+if (numberfirst < numbersekond);
+{
+    max = numbersekond;
+    min = numberfirst;
+}
+Console.WriteLine($"максимальное число: {max}");
+Console.WriteLine($"минимальное число: {min}");
