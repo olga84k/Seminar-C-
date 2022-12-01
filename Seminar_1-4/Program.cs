5479591d1e5388c740f03952a948fb6f7cf8725e
Olga <olga84k@mail.ru>
2022-12-01 15:01:40 +0400

    event numbers N

---
+++ Seminar_1-4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите  число");
+string number1 = Console.ReadLine() ?? "";
+int N = Convert.ToInt32(number1);
+int number = 1;
+if (N>0)
+{
+   while (number < N)
+{
+   int rem = number%2; 
+    if (rem==0)
+    {
+       Console.Write($"  {number}"); 
+    }
+    number = number + 1;
+}
+}
+else
+{
+   Console.WriteLine("не принадлежит области определения");
+}
