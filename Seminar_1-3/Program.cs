2c8d0af6183316d665e02e6cd58552e9e85cdac7
Olga <olga84k@mail.ru>
2022-12-01 11:35:58 +0400

    even number

---
+++ Seminar_1-3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите  число");
+string number1 = Console.ReadLine() ?? "";
+int number = Convert.ToInt32(number1);
+int rem = number%2;
+if (rem==0)
+
+    Console.WriteLine("Число четное"); 
+    else
+    
+        Console.WriteLine("Число нечетное"); 
