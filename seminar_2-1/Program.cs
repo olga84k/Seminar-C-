c0067ec4b20ff1b608d025a20e5e1c07a2aa5233
Olga <olga84k@mail.ru>
2022-12-05 10:34:35 +0400

    write_ten

---
+++ seminar_2-1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+string number1 = Console.ReadLine() ?? "";
+int number = Convert.ToInt32(number1);
+int result = ((number/10)%10);
+Console.WriteLine($"{result}");
