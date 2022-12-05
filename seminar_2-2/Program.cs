22400ce1cb4d0b2ba94941f1d22adaf6e62f4de7
Olga <olga84k@mail.ru>
2022-12-05 14:06:27 +0400

    2 index_number

---
+++ seminar_2-2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine($"Введите число");
+string number = Console.ReadLine() ?? "";
+string str = number.ToString();
+if (str.Length>=3)
+{
+  Console.WriteLine($"{str[2]}");  
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+} 
