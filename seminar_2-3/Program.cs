be1021e84d503c31bc45a1ca8b7637c087a478ae
Olga <olga84k@mail.ru>
2022-12-06 10:06:42 +0400

    frei day

---
+++ seminar_2-3/Program.cs
@@ -0,0 +1,10 @@
+﻿int number =  new Random().Next(1,8);
+Console.WriteLine($"{number}");
+if (number==6||number==7)
+{
+    Console.WriteLine("выходной день"); 
+}
+else
+{
+   Console.WriteLine("невыходной день"); 
+}
