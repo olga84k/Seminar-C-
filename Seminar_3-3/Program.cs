cc1d2992cf625dd1a62b75a7b5189cafd77c662c
Olga <olga84k@mail.ru>
2022-12-07 10:21:54 +0400

    number^3

---
+++ Seminar_3-3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N>0)
+{
+for(int i = 1;i <= N; i++)
+{
+Console.WriteLine($"{i * i* i}");
+}
+}
+else
+{
+  for(int i = N;i < 0; i++)
+{
+Console.WriteLine($"{i * i* i}");
+}  
+}
