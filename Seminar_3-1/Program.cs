97a57405a7da494b5a7fe76f5eb0237cfa613c94
Olga <olga84k@mail.ru>
2022-12-07 09:02:33 +0400

    palindrom

---
+++ Seminar_3-1/Program.cs
@@ -0,0 +1,11 @@
+﻿int number =  new Random().Next(10000,100000);
+Console.WriteLine($"{number}");
+string str = number.ToString();
+if (str[0]==str[4] && str[1]==str[3])
+{
+  Console.WriteLine($" {number} палиндром");  
+}
+else
+{
+    Console.WriteLine($" {number} непалиндром");
+} 
