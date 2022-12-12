3f023c65be4ccb0a20b2d14ead2b8a27b3bed33c
Olga <olga84k@mail.ru>
2022-12-12 13:53:34 +0400

    Array[8 numbers]

---
+++ seminar_4-3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] numbers = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+numbers[i] = rnd.Next(0, 10);
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+Console.Write(numbers[i] +" ");
+}
