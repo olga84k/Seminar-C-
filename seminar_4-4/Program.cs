e0c8c51367ef43d275888b9b0084b0d19842c4e7
Olga <olga84k@mail.ru>
2022-12-13 16:23:06 +0400

    summ number

---
+++ seminar_4-4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+string str = Console.ReadLine() ?? "";
+if (!int.TryParse(str, out int val)) return;
+int summ = 0;
+for (int i = 0; i < str.Length; i++)
+   {
+    summ = summ + int.Parse(str[i].ToString());
+   } 
+Console.WriteLine($"{summ}");
