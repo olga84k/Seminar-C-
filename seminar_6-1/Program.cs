84f1b9004d58c0d1d1ccdec2cf0f7bd19e0c9fd5
Olga <olga84k@mail.ru>
2022-12-19 16:03:40 +0400

    Formation

--- seminar_6-1/Program.cs
+++ seminar_6-1/Program.cs
@@ -4,39 +4,39 @@ int GetNumber(string message)
 int result;
 while(true)
 {
-Console.WriteLine(message);
+    Console.WriteLine(message);
 
-if(int.TryParse(Console.ReadLine(), out result))
-{
-break;
-}
-else
+    if(int.TryParse(Console.ReadLine(), out result))
+        {
+        break;
+        }
+    else
 {
 Console.WriteLine("Ввели не число.");
 }
-}
+        }
 
-return result;
+        return result;
 }
 
 int[] InitArray(int dimension)
 {
-int[] array = new int[dimension];
+    int[] array = new int[dimension];
 
-for (int i = 0; i < dimension; i++)
-{
-array[i] = Convert.ToInt32(Console.ReadLine());;
-}
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());;
+    }
 
 return array;
 }
 
 void PrintArray(int[] array)
 {
-for (int i = 0; i < array.Length; i++)
-{
-Console.Write($"{array[i]} ");
-}
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
 Console.WriteLine();
 }
 
@@ -47,9 +47,9 @@ int count = 0;
 
 for (int i =0; i < array.Length; i++)
 {
-if (array[i]>0)
-{
-    count++;
-}
+    if (array[i]>0)
+    {
+        count++;
+    }
 }
 Console.WriteLine($"{count}");
