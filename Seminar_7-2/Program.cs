842b6495f81e6ca6a5a2700c87bed43630f52238
Olga <olga84k@mail.ru>
2022-12-21 13:25:32 +0400

    двумерный массив,наличие в массиве элемента

---
+++ Seminar_7-2/Program.cs
@@ -0,0 +1,67 @@
+﻿//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintInitMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int rows = GetNumber("Введите количество строк:");
+int columns = GetNumber("Введите количество столбцов:");
+int[,] matrix = InitMatrix(rows, columns);
+PrintInitMatrix(matrix);
+Console.WriteLine("Введите индекс строки");
+int index1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца");
+int index2 = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+if (index1 <= rows && index2 <= columns)
+{
+    number = matrix[index1, index2];
+    Console.WriteLine($"{number}");
+}
+else
+{
+    Console.WriteLine($"{index1}{index2} такого числа в массиве нет");
+}
