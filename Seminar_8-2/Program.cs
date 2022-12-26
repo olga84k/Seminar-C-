6cc810d4ca51683e8d36e5584abeda2a751e4335
Olga <olga84k@mail.ru>
2022-12-26 16:47:19 +0400

    Сортировка строк по минимальной сумме строки

---
+++ Seminar_8-2/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintInitMatrix(int[,] matrix )
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int rows = GetNumber("Введите количество строк:");
+int columns = GetNumber("Введите количество столбцов:");
+int[,] matrix = InitMatrix(rows, columns);
+PrintInitMatrix(matrix);
+int summ = 0;
+int minsumm = 0;
+int str = 0;
+for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minsumm = minsumm + matrix[0,i];
+    }
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            summ = summ + matrix[i,j];
+        }
+        if (minsumm > summ)
+            {
+            minsumm = summ; 
+             str = i;
+             }
+            else
+            {
+            summ = 0; 
+            }
+
+        } 
+Console.WriteLine($"сумма {minsumm}, {str+1} строка");  
